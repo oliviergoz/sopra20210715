d1bf4148bb0225bbdffe0e1a8d60724f27f0411f
oliviergoz <o.gozlan@ajc-ingenierie.fr>
2021-07-23 16:52:30 +0200

    conflict

--- maven1/src/main/java/maven1/App.java
+++ maven1/src/main/java/maven1/App.java
@@ -4,7 +4,7 @@ public class App {
 	public static void main(String[] args) {
 
 		//commentaire sur la machine de pierrick
-		System.out.println("bonjour");
+		System.out.println("bonjour olivier");
 		//commentaire olivier
 	}
 }
