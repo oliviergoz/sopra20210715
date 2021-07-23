5e2a0580b7cf449274a42a60930287220ec68226
Unknown <o.gozlan@ajc-ingenierie.fr>
2021-07-23 16:40:21 +0200

    fichier modifier apres probleme

--- maven1/src/main/java/maven1/App.java
+++ maven1/src/main/java/maven1/App.java
@@ -3,7 +3,7 @@ package maven1;
 public class App {
 	public static void main(String[] args) {
 		//commentaire sur la machine de pierrick
-		System.out.println("hello");
+		System.out.println("bonjour");
 		//commentaire olivier
 	}
 }
