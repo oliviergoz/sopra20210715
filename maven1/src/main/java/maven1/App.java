47e899773370b70c491619ecd95eb582f78f2765
MiniBel <pierrickbellanger@hotmail.fr>
2021-07-23 16:24:06 +0200

    ajout commentaire pierrick

--- maven1/src/main/java/maven1/App.java
+++ maven1/src/main/java/maven1/App.java
@@ -2,7 +2,6 @@ package maven1;
 
 public class App {
 	public static void main(String[] args) {
-		//commentaire sur la machine de pierrick
 		System.out.println("hello world");
 		//commentaire olivier
 	}
